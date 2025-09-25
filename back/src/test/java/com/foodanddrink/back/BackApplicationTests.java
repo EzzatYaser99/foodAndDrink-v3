a56353252b355510b12e89eb95d22e709c01edaf
Ezzat Yaser <eyasser@ntgclarity.com>
2025-09-25 16:18:22 +0300

    create modules back ,front

---
+++ back/src/test/java/com/foodanddrink/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodanddrink.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
