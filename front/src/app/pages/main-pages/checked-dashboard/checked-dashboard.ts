b631d2762c307841eac785ef32a104ae3a578649
Ezzat Yaser <eyasser@ntgclarity.com>
2026-01-17 17:51:36 +0200

    create page(booking , checked-dashborad ,order-status ,pos , transaction)

---
+++ front/src/app/pages/main-pages/checked-dashboard/checked-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checked-dashboard',
+  imports: [],
+  templateUrl: './checked-dashboard.html',
+  styleUrl: './checked-dashboard.scss'
+})
+export class CheckedDashboard {
+
+}
