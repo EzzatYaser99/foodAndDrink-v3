63217fa1e53cf70b6f577cb09eaf728dee98c1cd
Ezzat Yaser <eyasser@ntgclarity.com>
2026-02-10 15:18:24 +0200

    adjust social-media component and use design system

--- front/src/app/core/components/social-media-info/social-media-info.component.ts
+++ front/src/app/core/components/social-media-info/social-media-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { DockModule } from 'primeng/dock';
 import { Tooltip } from 'primeng/tooltip';
+import { Language } from '../../enums/language.enum';
 
 @Component({
   selector: 'app-social-media-info',
@@ -12,7 +13,7 @@ import { Tooltip } from 'primeng/tooltip';
 export class SocialMediaInfoComponent {
   socialMediaInfo: SocialMediaInfo[] = [];
 
-  @Input() lang: 'en' | 'ar' = 'en';
+  @Input() lang: Language = Language.EN;
 
   constructor() {
     this.socialMediaInfo = [
@@ -20,21 +21,24 @@ export class SocialMediaInfoComponent {
         id: 1,
         titleEN: 'Facebook',
         titleAR: 'فيسبوك',
-        icon: 'fa-brands fa-facebook-f facebook"',
+        icon: 'fa-brands fa-facebook-f',
+        class: 'facebook-bg',
         href: 'https://www.facebook.com/ezzatyaser99?mibextid=ZbWKwL',
       },
       {
         id: 2,
         titleEN: 'Whatsapp',
         titleAR: 'واتساب',
-        icon: 'fa-brands fa-whatsapp whatsapp',
+        icon: 'fa-brands fa-whatsapp',
+        class: 'whatsapp-bg',
         href: 'https://wa.me/01554520650',
       },
       {
         id: 3,
         titleEN: 'instagram',
         titleAR: 'إنستغرام',
-        icon: 'fa-brands fa-instagram instagram',
+        icon: 'fa-brands fa-instagram',
+        class: 'instagram-bg',
         href: 'https://www.instagram.com/ezzat_yasser99?igsh=NGNzZmFreGEwMnp4',
       },
     ];
@@ -43,6 +47,7 @@ export class SocialMediaInfoComponent {
 export class SocialMediaInfo {
   id!: number;
   icon!: string;
+  class!: string;
   titleEN!: string;
   titleAR!: string;
   href!: string;
