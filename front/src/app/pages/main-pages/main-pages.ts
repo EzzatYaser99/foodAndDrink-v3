1b97526c56454fec9371d9a326f7ca751b13ceb1
Ezzat Yaser <eyasser@ntgclarity.com>
2026-02-10 15:25:32 +0200

    add sidebar to main pages

--- front/src/app/pages/main-pages/main-pages.ts
+++ front/src/app/pages/main-pages/main-pages.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Footer } from '../../core/components/footer/footer';
 import { Navbar } from '../../core/components/navbar/navbar';
+import { Sidebar } from '../../core/components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
 import { Spinner } from '../../core/components/spinner/spinner';
 
 @Component({
   selector: 'app-main',
-  imports: [Footer, Navbar, RouterOutlet, Spinner],
+  imports: [Footer, Navbar, Sidebar, RouterOutlet, Spinner],
   templateUrl: './main-pages.html',
   styleUrl: './main-pages.scss',
 })
 export class MainPages implements OnInit {
   isLoading: boolean = true;
+  sidebarOpen: boolean = false;
 
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = false;
     }, 1500);
+  }
+
+  onToggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
   }
 }
